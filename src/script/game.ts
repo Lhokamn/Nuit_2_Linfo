652b2364967297fb42613c4180916bcd5e8f12e1
Simon <simon-menard@etu.univ-lyon1.fr>
2022-12-01 23:57:54 +0100

    created const for stats

--- src/script/game.ts
+++ src/script/game.ts
@@ -1,11 +1,19 @@
+const investSidaction = document.getElementById("sidaction-button");
+const totalPts = document.getElementById("totalpts");
+const sidactionPts = document.getElementById("sidaction-pts");
+const pasteurPts = document.getElementById("pasteur-pts");
+const inseePts = document.getElementById("insee-pts");
+const letalityp = document.getElementById("letality");
+const transmissionpù = document.getElementById("transimssion");
+const dataQuality = document.getElementById("data-quality");
+const deathsp = document.getElementById("deaths");
+
 let points:number;
 let spentPointsSidaction:number;
 let spentPointsPasteur:number;
 let spentPointsInsee:number;
-let casualties:number;
+let deaths:number;
 let letality:number;
 let transmission:number;
 let data:number;
 let rank:number;
-
-const investSidaction = document.getElementById("sidaction-button");
