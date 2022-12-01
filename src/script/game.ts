a55252fb0fee03f7c56147549d7d66ee6d402f57
Simon <simon-menard@etu.univ-lyon1.fr>
2022-12-01 22:22:56 +0100

    created variables

---
+++ src/script/game.ts
@@ -0,0 +1,8 @@
+let points:number;
+let spentPointsSidaction:number;
+let spentPoints2:number;
+let spentPoints3:number;
+let peopleSaved:number;
+let rank:number;
+
+const investSidaction = document.getElementById("sidaction-button");
