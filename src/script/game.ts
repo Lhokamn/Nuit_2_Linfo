62f1b1dfa62d4236054496d97487f0b2a9399eca
Simon <simon-menard@etu.univ-lyon1.fr>
2022-12-01 23:18:05 +0100

    added variables casualties, letality, transmission, data

--- src/script/game.ts
+++ src/script/game.ts
@@ -2,7 +2,10 @@ let points:number;
 let spentPointsSidaction:number;
 let spentPointsPasteur:number;
 let spentPoints3:number;
-let peopleSaved:number;
+let casualties:number;
+let letality:number;
+let transmission:number;
+let data:number;
 let rank:number;
 
 const investSidaction = document.getElementById("sidaction-button");
